d87dbabb509a0ac0d1d820b581b47af20b30139e
Ankan Manna <149871785+ankan-manna@users.noreply.github.com>
2025-02-09 19:07:43 +0530

    Time: 0 ms (100%), Space: 10.3 MB (8.68%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -2,17 +2,15 @@ class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
         int n=matrix.size();
-        vector<vector<int>>v(n,vector<int>(n,0));
+       // vector<vector<int>v(n,vector<int>(n,0));
         for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                v[j][n-i-1]=matrix[i][j];
+            for(int j=0;j<i;j++){//cheak condition
+               swap(matrix[i][j],matrix[j][i]);//transpose
             }
         }
-
         for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                matrix[i][j]=v[i][j];
-            }
+            reverse(matrix[i].begin(),matrix[i].end());
         }
+        
     }
 };
