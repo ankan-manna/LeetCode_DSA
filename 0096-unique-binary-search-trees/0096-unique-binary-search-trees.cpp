eb112ae37ca1f6a1c0a619e3b18a232b21b9734e
Ankan Manna <149871785+ankan-manna@users.noreply.github.com>
2025-04-04 13:04:48 +0530

    Time: 0 ms (100%), Space: 7.9 MB (61.75%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
@@ -1,16 +1,32 @@
 class Solution {
+    
 public:
     // int solve(int n){
     //     if(n==1||n==0) return 1;
     //   return solve(n-1);
 
-    // }
-    int numTrees(int n) {
-        if(n==0 || n==1) return 1;
+    // }=> why not we call solve ===> when we call like that
+    /*
+for(int i=0;i<n;i++){
+    cnt+=solve(i)*solve(n-1-i);
+}
+n=10
+i=0 => s(0)*s(9)==> for 9 further we need another loop for(i=0;i<9;i++)
+which is not possible by calling solve 
+    */
+
+    int solve(int n,vector<int>&dp){
+         if(n==0 || n==1) return dp[n]=1;
+        if(dp[n]!=-1) return dp[n];
         int cnt=0;
       for(int i=0;i<n;i++){
-        cnt=cnt+(numTrees(i)*numTrees(n-1-i));
+        cnt=cnt+(solve(i,dp)*solve(n-1-i,dp));
+        
       }
-      return cnt;
+      return dp[n]=cnt;
+    }
+    int numTrees(int n) {
+       vector<int>dp(n+1,-1);
+       return solve(n,dp);
     }
 };
